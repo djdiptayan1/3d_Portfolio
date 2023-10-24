a542dd263773b94bc01ec2b333a6251121af9961
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-10-25 02:39:18 +0530

    added padding

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -48,12 +48,12 @@ const ProjectCard = ({
           </div>
         </div>
 
-        <div className="mt-5">
+        <div className="mt-5 px-2">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          <p className="mt-2 text-secondary text-[16px]">{description}</p>
         </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
+        <div className=" p-2 mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
