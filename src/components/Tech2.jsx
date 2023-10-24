92c888850a296dfe75faf1aec5e00202e5ebd6fa
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-10-24 21:35:00 +0530

    fixed grid issue in mobile

--- src/components/Tech2.jsx
+++ src/components/Tech2.jsx
@@ -14,7 +14,7 @@ const TechStack = () => {
             className="py-2"
         >
             <div className="flex flex-row flex-wrap justify-center gap-10">
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-10">
+                <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-6 gap-10">
                     {technologies.map((technology) => (
                         <Tilt
                             key={technology.name}
@@ -25,12 +25,12 @@ const TechStack = () => {
                             }}
                         >
                             <div
-                                className="ironman bg-white p-4 bg-opacity-25 rounded-full h-28 w-28 shadow-lg flex items-center justify-center text-center text-white transform transition-transform hover:scale-125"
+                                className="ironman bg-white p-4 bg-opacity-25 rounded-full h-20 w-20 lg:h-28 lg:w-28 md:h-28 md:w-28 shadow-lg flex items-center justify-center text-center text-white transform transition-transform hover:scale-125"
                                 style={{
                                     boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.75)", // Add a shadow to the perimeter of the logos
                                 }}
                             >
-                                <img src={technology.icon} className="h-16 mx-auto" />
+                                <img src={technology.icon} className="lg:h-16 md:h-20 mx-auto" />
                             </div>
                         </Tilt>
                     ))}
