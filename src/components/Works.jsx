3c5d013a0c7944a0c1919b0573e714413b065357
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-10-24 21:56:23 +0530

    added "View More Projects" github link

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -85,7 +85,7 @@ const Works = () => {
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          and manage projects effectively.<span className="text-blue-400"><a className="nav-cursor" href="https://github.com/djdiptayan1" target="_blank">  View All Projects...</a></span>
         </motion.p>
       </div>
 
