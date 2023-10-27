2fbcc6514236f373317aea145ed8b8d9be04e2bd
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-10-27 19:18:03 +0530

    commented out the loader

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,27 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      {loading ? (
+      {/* {loading ? (
         <Loading_Screen />
-      ) : (
-        <div className="relative z-0 bg-primary">
-          <Analytics />
-          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar2 />
-            <Hero />
-          </div>
-          <About />
-          <Experience />
-          <Tech2 />
-          <Works />
-          {/* <Feedbacks /> */}
-          <div className="relative z-0">
-            <Contact />
-            <StarsCanvas />
-          </div>
-          <StarsCanvas_bg />
+      ) : ( */}
+      <div className="relative z-0 bg-primary">
+        <Analytics />
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar2 />
+          <Hero />
         </div>
-      )}
+        <About />
+        <Experience />
+        <Tech2 />
+        <Works />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+        <StarsCanvas_bg />
+      </div>
+      {/* )} */}
     </BrowserRouter >
   );
 };
