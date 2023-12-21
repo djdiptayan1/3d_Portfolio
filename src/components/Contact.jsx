5289ffc7b504574dfd2d1c3e091a28ce3bde97e6
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-21 17:05:03 +0530

    Update social media links in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,6 @@ import { slideIn } from "../utils/motion";
 import "../Cursor.css";
 import { a } from "maath/dist/objectSpread2-284232a6.esm";
 
-
 const Contact = () => {
   const formref = useRef();
   const [form, setForm] = useState({
@@ -84,30 +83,67 @@ const Contact = () => {
         <div className="rounded-xl relative text-center p-1 group items-center flex flex-col max-w-sm">
           <div className="flex items-center transition-all duration-500 delay-200 w-full">
             <div className="flex gap-3 text-2xl bg-gray-700 text-white p-1 transition-all duration-500 delay-200 rounded-full shadow-sm">
-              <a href="https://github.com/djdiptayan1" target="_blank" className="nav-cursor">
-                <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 1024 1024">
+              <a
+                href="https://github.com/djdiptayan1"
+                target="_blank"
+                className="nav-cursor"
+              >
+                <svg
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  viewBox="0 0 1024 1024"
+                >
                   <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path>
                 </svg>
               </a>
-              <a href="https://www.instagram.com/dj_dipt/" target="_blank" className="nav-cursor">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 50 50" className="invert">
+              <a
+                href="https://www.instagram.com/dj_dipt/"
+                target="_blank"
+                className="nav-cursor"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 50 50"
+                  className="invert"
+                >
                   <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
                 </svg>
               </a>
-              <a href="https://www.linkedin.com/in/diptayan-jash-63496b214/" target="_blank" className="nav-cursor">
-                <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 960 1000">
+              <a
+                href="https://www.linkedin.com/in/diptayan-jash-63496b214/"
+                target="_blank"
+                className="nav-cursor"
+              >
+                <svg
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  viewBox="0 0 960 1000"
+                >
                   <path d="M480 20c133.333 0 246.667 46.667 340 140s140 206.667 140 340c0 132-46.667 245-140 339S613.333 980 480 980c-132 0-245-47-339-141S0 632 0 500c0-133.333 47-246.667 141-340S348 20 480 20M362 698V386h-96v312h96m-48-352c34.667 0 52-16 52-48s-17.333-48-52-48c-14.667 0-27 4.667-37 14s-15 20.667-15 34c0 32 17.333 48 52 48m404 352V514c0-44-10.333-77.667-31-101s-47.667-35-81-35c-44 0-76 16.667-96 50h-2l-6-42h-84c1.333 18.667 2 52 2 100v212h98V518c0-12 1.333-20 4-24 8-25.333 24.667-38 50-38 32 0 48 22.667 48 68v174h98"></path>
                 </svg>
               </a>
-              <a href="mailto:djdiptayan1@gmail.com" target="_blank" className="nav-cursor">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 50 50" className="invert">
+              <a
+                href="mailto:djdiptayan1@gmail.com"
+                target="_blank"
+                className="nav-cursor"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 50 50"
+                  className="invert"
+                >
                   <path d="M 5.5 7 C 3.03125 7 1.011719 9.015625 1 11.484375 C 1 11.484375 1 11.488281 1 11.492188 C 1 11.496094 1 11.496094 1 11.5 L 1 38.5 C 1 40.972656 3.027344 43 5.5 43 L 44.5 43 C 46.972656 43 49 40.972656 49 38.5 L 49 11.5 C 49 11.496094 49 11.496094 49 11.492188 C 49 11.488281 49 11.484375 49 11.484375 C 48.988281 9.015625 46.96875 7 44.5 7 Z M 8.101563 9 L 41.902344 9 L 25 20.78125 Z M 4.773438 9.117188 L 25 23.21875 L 45.230469 9.117188 C 46.253906 9.425781 46.992188 10.355469 47 11.488281 C 46.996094 11.699219 46.78125 12.121094 46.46875 12.460938 C 46.152344 12.804688 45.84375 13.019531 45.84375 13.019531 L 45.839844 13.027344 L 25 27.777344 L 4.160156 13.027344 L 4.15625 13.019531 C 4.15625 13.019531 3.847656 12.804688 3.53125 12.460938 C 3.21875 12.121094 3.003906 11.699219 3 11.488281 C 3.007813 10.355469 3.746094 9.425781 4.773438 9.117188 Z M 3 14.652344 C 3 14.652344 3.007813 14.660156 3.007813 14.660156 L 3.015625 14.664063 L 3.015625 14.667969 L 6 16.777344 L 6 41 L 5.5 41 C 4.109375 41 3 39.890625 3 38.5 Z M 47 14.652344 L 47 38.5 C 47 39.890625 45.890625 41 44.5 41 L 44 41 L 44 16.777344 L 46.984375 14.667969 L 46.984375 14.664063 C 46.984375 14.664063 47 14.652344 47 14.652344 Z M 8 18.191406 L 25 30.222656 L 42 18.191406 L 42 41 L 8 41 Z"></path>
                 </svg>
               </a>
             </div>
           </div>
         </div>
-
 
         <form
           ref={formref}
