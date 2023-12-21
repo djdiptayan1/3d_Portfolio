80fa1a91cf95fad62234949c916e7e8bfb3b75de
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-22 00:56:07 +0530

    Remove unused code and loading screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
-import React, { useState, useEffect } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
 import {
   About,
   Contact,
   Experience,
-  Feedbacks,
   Hero,
   Navbar2,
-  Tech,
   Tech2,
   Works,
   StarsCanvas,
   StarsCanvas_bg,
 } from "./components";
-import Loading_Screen from "./components/Loading_Screen";
 import { useMediaQuery } from "react-responsive";
 import "./Cursor.css";
 const App = () => {
   const isDesktop = useMediaQuery({ minWidth: 992 });
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Change this delay as needed
-  }, []);
 
   return (
     <BrowserRouter>
-      {/* {loading ? (
-        <Loading_Screen />
-      ) : ( */}
       <div className="relative z-0 bg-primary">
         <Analytics />
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
