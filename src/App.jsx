dc25daecaad88397239e836a9fe8f85a3e800100
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-25 15:38:46 +0530

    made the hero img transparent, BG_stars visible only in desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar2, Tech, Tech2, Works, StarsCanvas, StarsCanvas_bg } from "./components";
-import Loading_Screen from './components/Loading_Screen';
-import './Cursor.css';
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar2,
+  Tech2,
+  Works,
+  StarsCanvas,
+  StarsCanvas_bg,
+} from "./components";
+import "./Cursor.css";
+import { useMediaQuery } from "react-responsive";
+
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000); // Change this delay as needed
-  }, []);
-
+  const isDesktop = useMediaQuery({ minWidth: 992 });
   return (
     <BrowserRouter>
-      {/* {loading ? (
-        <Loading_Screen />
-      ) : ( */}
       <div className="relative z-0 bg-primary">
         <Analytics />
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="bg-hero-pattern_transparent bg-cover bg-no-repeat bg-center">
           <Navbar2 />
           <Hero />
         </div>
@@ -28,15 +28,13 @@ const App = () => {
         <Experience />
         <Tech2 />
         <Works />
-        {/* <Feedbacks /> */}
+        <div> {isDesktop && <StarsCanvas_bg />}</div>
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
-        <StarsCanvas_bg />
       </div>
-      {/* )} */}
-    </BrowserRouter >
+    </BrowserRouter>
   );
 };
-export default App
+export default App;
